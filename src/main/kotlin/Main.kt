6132e6a049632bb61e0660b54f9c2973367db81a
cko003 <cko003@bucknell.edu>
2024-06-24 16:02:37 +0100

    functions practice exercise 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,33 @@
 package com.chianugo
 import java.util.*
-fun getFortuneCookie():String? {
-    val fortunes = listOf("You will have a great day!", "Take it easy","Things will go well for you today.", "Today is a good day for exercising restraint.", "Treasure your friends because they are your greatest fortune.")
-    print("Enter your age:")
-    val age:Int? = readlnOrNull()?.toIntOrNull()
-    if (age == null) {
-        return "null"
-    }
-    return fortunes[age % fortunes.size]
-}
 fun main(args: Array<String>) {
-    var x:String?
-    for (i in 1..10) {
-        x = getFortuneCookie()
-        println("Your fortune is: ${x}")
-        if (x == "Take it easy") {
-            break
-        }
+    println("Hello")
+    feedTheFish()
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+    var food = "fasting"
+
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
     }
+
+    return food
 }
