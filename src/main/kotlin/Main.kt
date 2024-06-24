61eb3206541a835b8482ffd973e6c0525a69c7cb
cko003 <cko003@bucknell.edu>
2024-06-24 15:44:29 +0100

    functions practice exercise 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,15 @@
 package com.chianugo
 import java.util.*
-fun dayOfWeek() {
-    println("What day is it today?")
-    print("Today is ")
-    when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
-        1 -> println("Sunday")
-        2 -> println("Monday")
-        3 -> println("Tuesday")
-        4 -> println("Wednesday")
-        5 -> println("Thursday")
-        6 -> println("Friday")
-        7 -> println("Saturday")
-        else -> println("I don't know what today is")
+fun getFortuneCookie():String? {
+    val fortunes = listOf("You will have a great day!", "Things will go well for you today.", "Today is a good day for exercising restraint.", "Treasure your friends because they are your greatest fortune.")
+    print("Enter your age:")
+    val age:Int? = readlnOrNull()?.toIntOrNull()
+    if (age == null) {
+        return "null"
     }
+    return fortunes[age % fortunes.size]
 }
 fun main(args: Array<String>) {
-    dayOfWeek()
+    println("Your fortune is: ${getFortuneCookie()}")
 
 }
